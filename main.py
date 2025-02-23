b3147568e205de38b5b1e25add6a6acd3d0c06f2
Khatanbuuvei Bold <khatna@github.com>
2025-02-23 17:14:35 +0000

    add types and cors policy

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 from lib import market_store
-
-from lib.models import MarketSeedData
-
-class Bet(BaseModel):
-  market_id: str
-  is_yes: bool # True for buy, false for sell
-  stake: float
+from lib.models import MarketSeedData, MarketData, Bet, BetResponse
 
 market_store = market_store.MarketStore()
 
@@ -26,18 +20,26 @@ for q in questions:
       initial_probability=0.5
     ),
   )
+  
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/markets")
-def read_markets():
+def read_markets() -> list[MarketData]:
   return market_store.get_all_markets()
 
 @app.get("/markets/{market_id}")
-def read_market(market_id: str):
+def read_market(market_id: str) -> MarketData:
   return market_store.get_market(market_id)
 
 @app.post("/bet")
-def make_bet(bet: Bet):
+def make_bet(bet: Bet) -> BetResponse:
   purchased = market_store.market_bet(bet.market_id, bet.is_yes, bet.stake)
   market_data = market_store.get_market(bet.market_id)
   return { 'purchased': purchased, 'market_data': market_data }
